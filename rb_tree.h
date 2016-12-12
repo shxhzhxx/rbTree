77aef74e2bf8a325b31912a2301cafe205c6d506
SH <sh.ylzhxx@163.com>
2016-12-12 11:27:47 +0800

    a

--- rb_tree.h
+++ rb_tree.h
@@ -3,85 +3,154 @@
 
 #include <unistd.h>
 #include <pthread.h>
+#include <stdio.h>
 
 const int BLACK = 0;
 const int RED = 1;
 
-/*the node of the red&black tree*/
-template<class T>
-class node {
+
+
+
+class satellite {
 public:
-    node(long _key = 0, T *_pointer = 0) : key(_key), parent(0), left(0), right(0), color(BLACK), pointer(_pointer),
-                                           mutex(0) {
+    satellite(){
         mutex = new pthread_mutex_t();
-        pthread_mutex_init(mutex, NULL);
+        pthread_mutex_init(mutex,NULL);
     }
 
-    ~node() {
-        delete pointer;        //automatically release memory in heap
+    virtual ~satellite() {
         pthread_mutex_destroy(mutex);
         delete mutex;
     }
+    int mutex_trylock() { return pthread_mutex_trylock(mutex); }
 
     void mutex_lock() { pthread_mutex_lock(mutex); }
 
     void mutex_unlock() { pthread_mutex_unlock(mutex); }
+private:
+    pthread_mutex_t *mutex;
+};
+
+
+/*the node of the red&black tree*/
+class node {
+public:
+    node(long _key = 0, satellite *_p = 0) : key(_key), parent(0), left(0), right(0), color(BLACK), p(_p){}
+
+    ~node() {
+        delete p;        //automatically release memory in heap
+    }
+
+    void free_tree(){
+        if(left)
+            left->free_tree();
+        if(right)
+            right->free_tree();
+        delete this;
+    }
 
     long key;
     node *parent;
     node *left;
     node *right;
     int color;
-    T *pointer;
-private:
-    pthread_mutex_t *mutex;
+
+    satellite *p;
 };
 
 
 /*the tree*/
-template<class T>
 class rb_tree {
 public:
     rb_tree() : nil(0), root(0), count(0), rwlock(0) {
-        nil = new node<T>();
+        nil = new node();
         root = nil;
         rwlock = new pthread_rwlock_t();
         pthread_rwlock_init(rwlock, NULL);
     }
 
     ~rb_tree() {
+        rwlock_wrlock();
+        if(root!=nil){
+            root->free_tree();
+        }
+        delete nil;
+        rwlock_unlock();
         pthread_rwlock_destroy(rwlock);
         delete rwlock;
-        delete nil;
     }
 
     /**
      * 查找一条数据，未找到时返回NULL，找到时返回指向数据的指针
      */
-    node<T> *search(long _key, bool auto_lock = false) {
+    satellite *search(long _key, bool auto_lock = false) {
         rwlock_rdlock();
-        node<T> *result = 0;
-        int result_code = rb_search(_key, &result);
-        if (result_code == 1){
+        node *result = 0;
+        if (rb_search(_key, &result) == -1){
         	rwlock_unlock();
             return NULL;
         }
         if (auto_lock){
-            result->mutex_lock();
+            result->p->mutex_lock();
         }
         rwlock_unlock();
-        return result;
+        return result->p;
+    }
+
+    /**
+     * 查找数据，
+     * lock=0 不上锁           找到数据时返回0，否则返回-1
+     * lock=1 上锁             找到数据时返回0，否则返回-1   可能阻塞
+     * lock=2 尝试上锁         未找到数据时返回-1，找到数据但上锁失败时返回1，找到数据并上锁成功时返回0
+     */
+    int search_try(long _key, satellite **result,int lock=0){
+        rwlock_rdlock();
+        node *node_p;
+        if (rb_search(_key, &node_p) == -1){
+            rwlock_unlock();
+            return -1;
+        }
+        int ret=0;
+        if (lock==1){
+            node_p->p->mutex_lock();
+        }else if(lock==2 && node_p->p->mutex_trylock()!=0){
+            ret=1;
+        }
+        rwlock_unlock();
+        (*result)=node_p->p;
+        return ret;
     }
 
     /**
      * 插入一条数据，key重复时返回1，并覆盖原数据，正常时返回0
-     * result不为null时，将result指向新插入的数据
      */
-    int insert(long _key, T *data, node<T> **result = NULL, bool auto_lock = false) {
+    int insert(long _key, satellite *data, bool auto_lock = false) {
         rwlock_wrlock();
-        int result_code = rb_insert(_key, data, result);
+        int result_code = rb_insert(_key, data);
         if (auto_lock)
-            (*result)->mutex_lock();
+            data->mutex_lock();
+        if (result_code == 0)
+            count++;
+        rwlock_unlock();
+        return result_code;
+    }
+
+
+    /**
+     * 尝试插入一条数据，key重复时返回1，并覆盖原数据，
+     * 正常时返回0，
+     * key重复且插入（上锁）失败时返回-1，同时释放data的内存
+     */
+    int insert_try(long _key, satellite *data, bool auto_lock = false){
+        rwlock_wrlock();
+        int result_code = rb_insert_try(_key, data);
+        if(result_code==-1){//加锁失败
+            delete data;
+            return -1;
+        }
+        if (auto_lock){
+            data->mutex_lock();
+        }
         if (result_code == 0)
             count++;
         rwlock_unlock();
@@ -89,11 +158,11 @@ public:
     }
 
     /**
-     * 移除一条数据，成功时返回0，未找到指定数据时返回1
+     * 移除一条数据，成功时返回0，未找到指定数据时返回-1
      */
     int remove(long _key) {
         rwlock_wrlock();
-        node<T> *result = 0;
+        node *result = 0;
         if (rb_search(_key, &result) == 0) {
             rb_delete(result);
             count--;
@@ -101,46 +170,39 @@ public:
             return 0;
         } else {
             rwlock_unlock();
-            return 1;
+            return -1;
         }
     }
 
-    node<T> *get(int index, bool auto_lock = false) {
+    /**
+     *从小到大，返回第index个数据
+     */
+    satellite *get(int index, bool auto_lock = false) {
         rwlock_rdlock();
         if (root == nil || index >= count || index < 0) {
             rwlock_unlock();
             return NULL;
         }
-        node<T> *result = tree_minimum(root);
+        node *result = tree_minimum(root);
         while (index > 0) {
             result = tree_successor(result);
             index--;
         }
         if (auto_lock)
-            result->mutex_lock();
+            result->p->mutex_lock();
         rwlock_unlock();
-        return result;
+        return result->p;
     }
 
-    node<T> *getNext(node<T> *x, bool auto_lock = false) {
-        rwlock_rdlock();
-        node<T> *result = tree_successor(x);
-        if (result == nil) {
-            rwlock_unlock();
-            return NULL;
-        }
-        if (auto_lock)
-            result->mutex_lock();
-        rwlock_unlock();
-        return result;
+    int size(){
+        return count;
     }
 
-    int getCount() { return count; }
 
 
 private:
-    node<T> *nil;
-    node<T> *root;
+    node *nil;
+    node *root;
     int count;
     pthread_rwlock_t *rwlock;
 
@@ -152,30 +214,31 @@ private:
 
     void rb_reset_root();
 
-    void left_rotate(node<T> *x);
+    void left_rotate(node *x);
 
-    void right_rotate(node<T> *x);
+    void right_rotate(node *x);
 
-    void rb_insert_fixup(node<T> *z);
+    void rb_insert_fixup(node *z);
 
-    void rb_delete_fixup(node<T> *x);
+    void rb_delete_fixup(node *x);
 
-    int rb_delete(node<T> *z);
+    int rb_delete(node *z);
 
-    int rb_insert(long _key, T *data, node<T> **result);
+    int rb_insert(long _key, satellite *data);
 
-    int rb_search(long _key, node<T> **result);
+    int rb_insert_try(long _key, satellite *data);
 
-    node<T> *tree_minimum(node<T> *x);
+    int rb_search(long _key, node **result);
 
-    node<T> *tree_maximum(node<T> *x);
+    node *tree_minimum(node *x);
 
-    node<T> *tree_successor(node<T> *x);
+    node *tree_maximum(node *x);
+
+    node *tree_successor(node *x);
 };
 
 
-template<class T>
-inline void rb_tree<T>::rb_reset_root() {
+inline void rb_tree::rb_reset_root() {
     if (root == nil)
         return;
     while (root->parent != nil)
@@ -183,25 +246,22 @@ inline void rb_tree<T>::rb_reset_root() {
     return;
 }
 
-template<class T>
-inline node<T> *rb_tree<T>::tree_minimum(node<T> *x) {
+inline node *rb_tree::tree_minimum(node *x) {
     while (x->left != nil)
         x = x->left;
     return x;
 }
 
-template<class T>
-inline node<T> *rb_tree<T>::tree_maximum(node<T> *x) {
+inline node *rb_tree::tree_maximum(node *x) {
     while (x->right != nil)
         x = x->right;
     return x;
 }
 
-template<class T>
-inline node<T> *rb_tree<T>::tree_successor(node<T> *x) {
+inline node *rb_tree::tree_successor(node *x) {
     if (x->right != nil)
         return tree_minimum(x->right);
-    node<T> *y = x->parent;
+    node *y = x->parent;
     while (y != nil && x == y->right) {
         x = y;
         y = y->parent;
@@ -210,9 +270,8 @@ inline node<T> *rb_tree<T>::tree_successor(node<T> *x) {
 }
 
 
-template<class T>
-void rb_tree<T>::left_rotate(node<T> *x) {
-    node<T> *y;
+void rb_tree::left_rotate(node *x) {
+    node *y;
     y = x->right;
     x->right = y->left;
     if (y->left != nil)
@@ -231,9 +290,8 @@ void rb_tree<T>::left_rotate(node<T> *x) {
     rb_reset_root();
 }
 
-template<class T>
-void rb_tree<T>::right_rotate(node<T> *x) {
-    node<T> *y;
+void rb_tree::right_rotate(node *x) {
+    node *y;
     y = x->left;
     x->left = y->right;
     if (y->right != nil)
@@ -252,9 +310,8 @@ void rb_tree<T>::right_rotate(node<T> *x) {
     rb_reset_root();
 }
 
-template<class T>
-void rb_tree<T>::rb_insert_fixup(node<T> *z) {
-    node<T> *y;
+void rb_tree::rb_insert_fixup(node *z) {
+    node *y;
     while (z->parent->color == RED) {
         if (z->parent == z->parent->parent->left) {
             y = z->parent->parent->right;
@@ -293,16 +350,15 @@ void rb_tree<T>::rb_insert_fixup(node<T> *z) {
     root->color = BLACK;
 }
 
-template<class T>
-int rb_tree<T>::rb_insert(long _key, T *data, node<T> **result) {
-    node<T> *z = new node<T>(_key, data);
+int rb_tree::rb_insert(long _key, satellite *data) {
+    node *z = new node(_key, data);
     z->color = RED;
     z->left = nil;
     z->right = nil;
     z->parent = 0;
 
-    node<T> *y = nil;
-    node<T> *x = root;
+    node *y = nil;
+    node *x = root;
 
     while (x != nil) {
         y = x;
@@ -311,15 +367,12 @@ int rb_tree<T>::rb_insert(long _key, T *data, node<T> **result) {
         } else if (z->key > x->key) {
             x = x->right;
         } else {
-            x->mutex_lock();
-            x->mutex_unlock();
-            T *temp = x->pointer;
-            x->pointer = z->pointer;
-            z->pointer = temp;
+            x->p->mutex_lock();
+            x->p->mutex_unlock();
+            satellite *temp = x->p;
+            x->p = z->p;
+            z->p = temp;
             delete z;
-            if (result != NULL) {
-                *result = x;
-            }
             return 1;
         }
     }
@@ -334,16 +387,56 @@ int rb_tree<T>::rb_insert(long _key, T *data, node<T> **result) {
             y->right = z;
     }
     rb_insert_fixup(z);
-    if (result != NULL) {
-        *result = z;
+    return 0;
+}
+
+int rb_tree::rb_insert_try(long _key, satellite *data) {
+    node *z = new node(_key, data);
+    z->color = RED;
+    z->left = nil;
+    z->right = nil;
+    z->parent = 0;
+
+    node *y = nil;
+    node *x = root;
+
+    while (x != nil) {
+        y = x;
+        if (z->key < x->key) {
+            x = x->left;
+        } else if (z->key > x->key) {
+            x = x->right;
+        } else {
+            if(x->p->mutex_trylock()==0){
+                satellite *temp = x->p;
+                x->p = z->p;
+                z->p = temp;
+                delete z;
+                x->p->mutex_unlock();
+                return 1;
+            }else{
+                delete z;
+                return -1;
+            }   
+        }
     }
+    z->parent = y;
+
+    if (y == nil) {
+        root = z;
+    } else {
+        if (z->key < y->key)
+            y->left = z;
+        else
+            y->right = z;
+    }
+    rb_insert_fixup(z);
     return 0;
 }
 
 
-template<class T>
-void rb_tree<T>::rb_delete_fixup(node<T> *x) {
-    node<T> *w;
+void rb_tree::rb_delete_fixup(node *x) {
+    node *w;
     while (x != root && x->color == BLACK) {
         if (x == x->parent->left) {
             w = x->parent->right;
@@ -398,10 +491,9 @@ void rb_tree<T>::rb_delete_fixup(node<T> *x) {
     x->color = BLACK;
 }
 
-template<class T>
-int rb_tree<T>::rb_delete(node<T> *z) {
-    node<T> *y;
-    node<T> *x;
+int rb_tree::rb_delete(node *z) {
+    node *y;
+    node *x;
     if (z->left == nil || z->right == nil) {
         y = z;
     } else {
@@ -423,24 +515,23 @@ int rb_tree<T>::rb_delete(node<T> *z) {
         }
     }
     if (y != z) {
-        T *temp = z->pointer;
+        satellite *p_temp = z->p;
         z->key = y->key;
-        z->pointer = y->pointer;
-        y->pointer = temp;
+        z->p = y->p;
+        y->p = p_temp;
     }
     if (y->color == BLACK) {
         rb_delete_fixup(x);
     }
-    y->mutex_lock();
-    y->mutex_unlock();
+    y->p->mutex_lock();
+    y->p->mutex_unlock();
     delete y;
     return 0;
 }
 
 
-template<class T>
-int rb_tree<T>::rb_search(long _key, node<T> **result) {
-    node<T> *_root = root;
+int rb_tree::rb_search(long _key, node **result) {
+    node *_root = root;
     while (_root != nil && _root->key != _key) {
         if (_root->key > _key)
             _root = _root->left;
@@ -449,7 +540,7 @@ int rb_tree<T>::rb_search(long _key, node<T> **result) {
     }
     *result = _root;
     if (_root == nil) {
-        return 1;
+        return -1;
     } else {
         return 0;
     }
